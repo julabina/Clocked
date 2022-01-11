db08fb06ffa68a885c06156012da03c2cc755b37
Lenfumé Julien <julabina@hotmail.fr>
2022-01-11 21:34:16 +0100

    update readme and convert jpg to webp

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-https://julabina.github.io/Clocked/
+# CLOCKED
+
+The background changes with the seasons. 
+
+<https://julabina.github.io/Clocked/>
+
+![](./assets/screenshot1.webp)
+
+![](./assets/screenshot2.webp)
+
+## Credit picture
+
+- David Bartus
+- Kadri Vosumae
+- Nathan Timblin
+- Dzenina Lukac
+- Kris Møklebust
+- Guy Dwelly
+- Johannes Plenio
+- James Wheeler
+- Dimitry Anikin 
+- Tomáš Malík 
+- Jonathan Petersson
+- Mikhail Nilov 
+- Palo Cech
+- Nathan Cowley
+- Borislav Krustev 
+- eberhard grossgasteiger 
+- Bill White
+- Hrvoje Abraham Milićević 
+- Simon Berger 
+- Oleg Magni
+- Ramdas Ware 
+- Freddie Ramm
+- Mina-Marie Michell 
+- Saurav Sen
+- zhang kaiyv
+- Kaique Rocha 
+- Jonas Von Werne 
+- Anton Atanasov
+- Brett Sayles
+- Elina Fairytale 
+- Joonas kääriäinen 
+- Stephan Müller
+- Mitja Juraja 
+- Lennart Wittstock
+- joalfa from [flaticon.com](https://www.flaticon.com/fr/)
--- index.js
+++ index.js
@@ -34,100 +34,100 @@ const choseImg = () => {
       urll = "./assets/specialDay/newYear/" + randSpecial + ".jpg";
       changeBackground(urll);
     } else {
-      urll = "./assets/winter/" + rand10 + ".jpg";
+      urll = "./assets/winter/" + rand10 + ".webp";
       changeBackground(urll);
     }
   } else if (currentMonth === 1) {
     if (currentDate > 28) {
       daySelect.value = 28;
-      urll = "./assets/winter/" + rand10 + ".jpg";
+      urll = "./assets/winter/" + rand10 + ".webp";
       changeBackground(urll);
     } else {
-      urll = "./assets/winter/" + rand10 + ".jpg";
+      urll = "./assets/winter/" + rand10 + ".webp";
       changeBackground(urll);
     }
   } else if (currentMonth === 2) {
     if (currentDate > 20) {
-      urll = "./assets/spring/" + rand10 + ".jpg";
+      urll = "./assets/spring/" + rand10 + ".webp";
       changeBackground(urll);
     } else {
-      urll = "./assets/winter/" + rand10 + ".jpg";
+      urll = "./assets/winter/" + rand10 + ".webp";
       changeBackground(urll);
     }
   } else if (currentMonth === 3) {
     if (currentDate === 31) {
       daySelect.value = 30;
-      urll = "./assets/spring/" + rand10 + ".jpg";
+      urll = "./assets/spring/" + rand10 + ".webp";
       changeBackground(urll);
     } else {
-      urll = "./assets/spring/" + rand10 + ".jpg";
+      urll = "./assets/spring/" + rand10 + ".webp";
       changeBackground(urll);
     }
   } else if (currentMonth === 4) {
-    urll = "./assets/spring/" + rand10 + ".jpg";
+    urll = "./assets/spring/" + rand10 + ".webp";
     changeBackground(urll);
   } else if (currentMonth === 5) {
     if (currentDate > 20) {
       if (currentDate === 31) {
         daySelect.value = 30;
-        urll = "./assets/summer/" + rand10 + ".jpg";
+        urll = "./assets/summer/" + rand10 + ".webp";
         changeBackground(urll);
       } else {
-        urll = "./assets/summer/" + rand10 + ".jpg";
+        urll = "./assets/summer/" + rand10 + ".webp";
         changeBackground(urll);
       }
     } else {
-      urll = "./assets/spring/" + rand10 + ".jpg";
+      urll = "./assets/spring/" + rand10 + ".webp";
       changeBackground(urll);
     }
   } else if (currentMonth === 6) {
-    urll = "./assets/summer/" + rand10 + ".jpg";
+    urll = "./assets/summer/" + rand10 + ".webp";
     changeBackground(urll);
   } else if (currentMonth === 7) {
-    urll = "./assets/summer/" + rand10 + ".jpg";
+    urll = "./assets/summer/" + rand10 + ".webp";
     changeBackground(urll);
   } else if (currentMonth === 8) {
     if (currentDate > 20) {
       if (currentDate === 31) {
         daySelect.value = 30;
-        urll = "./assets/autumn/" + rand10 + ".jpg";
+        urll = "./assets/autumn/" + rand10 + ".webp";
         changeBackground(urll);
       } else {
-        urll = "./assets/autumn/" + rand10 + ".jpg";
+        urll = "./assets/autumn/" + rand10 + ".webp";
         changeBackground(urll);
       }
     } else {
-      urll = "./assets/summer/" + rand10 + ".jpg";
+      urll = "./assets/summer/" + rand10 + ".webp";
       changeBackground(urll);
     }
   } else if (currentMonth === 9) {
     if (currentDate === 31) {
       let randSpecial = random(5);
-      urll = "./assets/specialDay/halloween/" + randSpecial + ".jpg";
+      urll = "./assets/specialDay/halloween/" + randSpecial + ".webp";
       changeBackground(urll);
     } else {
-      urll = "./assets/autumn/" + rand10 + ".jpg";
+      urll = "./assets/autumn/" + rand10 + ".webp";
       changeBackground(urll);
     }
   } else if (currentMonth === 10) {
     if (currentDate === 31) {
       daySelect.value = 30;
-      urll = "./assets/autumn/" + rand10 + ".jpg";
+      urll = "./assets/autumn/" + rand10 + ".webp";
       changeBackground(urll);
     } else {
-      urll = "./assets/autumn/" + rand10 + ".jpg";
+      urll = "./assets/autumn/" + rand10 + ".webp";
       changeBackground(urll);
     }
   } else if (currentMonth === 11) {
     if (currentDate === 25) {
       let randSpecial = random(5);
-      urll = "./assets/specialDay/christmas/" + randSpecial + ".jpg";
+      urll = "./assets/specialDay/christmas/" + randSpecial + ".webp";
       changeBackground(urll);
     } else if (currentDate > 20) {
-      urll = "./assets/winter/" + rand10 + ".jpg";
+      urll = "./assets/winter/" + rand10 + ".webp";
       changeBackground(urll);
     } else {
-      urll = "./assets/autumn/" + rand10 + ".jpg";
+      urll = "./assets/autumn/" + rand10 + ".webp";
       changeBackground(urll);
     }
   }
@@ -149,61 +149,61 @@ const preloadImages = (images) => {
 };
 
 let linkImg = [
-  "./assets/spring/1.jpg",
-  "./assets/spring/2.jpg",
-  "./assets/spring/3.jpg",
-  "./assets/spring/4.jpg",
-  "./assets/spring/5.jpg",
-  "./assets/spring/6.jpg",
-  "./assets/spring/7.jpg",
-  "./assets/spring/8.jpg",
-  "./assets/spring/9.jpg",
-  "./assets/spring/10.jpg",
-  "./assets/summer/1.jpg",
-  "./assets/summer/2.jpg",
-  "./assets/summer/3.jpg",
-  "./assets/summer/4.jpg",
-  "./assets/summer/5.jpg",
-  "./assets/summer/6.jpg",
-  "./assets/summer/7.jpg",
-  "./assets/summer/8.jpg",
-  "./assets/summer/9.jpg",
-  "./assets/summer/10.jpg",
-  "./assets/autumn/1.jpg",
-  "./assets/autumn/2.jpg",
-  "./assets/autumn/3.jpg",
-  "./assets/autumn/4.jpg",
-  "./assets/autumn/5.jpg",
-  "./assets/autumn/6.jpg",
-  "./assets/autumn/7.jpg",
-  "./assets/autumn/8.jpg",
-  "./assets/autumn/9.jpg",
-  "./assets/autumn/10.jpg",
-  "./assets/winter/1.jpg",
-  "./assets/winter/2.jpg",
-  "./assets/winter/3.jpg",
-  "./assets/winter/4.jpg",
-  "./assets/winter/5.jpg",
-  "./assets/winter/6.jpg",
-  "./assets/winter/7.jpg",
-  "./assets/winter/8.jpg",
-  "./assets/winter/9.jpg",
-  "./assets/winter/10.jpg",
-  "./assets/specialDay/christmas/1.jpg",
-  "./assets/specialDay/christmas/2.jpg",
-  "./assets/specialDay/christmas/3.jpg",
-  "./assets/specialDay/christmas/4.jpg",
-  "./assets/specialDay/christmas/5.jpg",
-  "./assets/specialDay/halloween/1.jpg",
-  "./assets/specialDay/halloween/2.jpg",
-  "./assets/specialDay/halloween/3.jpg",
-  "./assets/specialDay/halloween/4.jpg",
-  "./assets/specialDay/halloween/5.jpg",
-  "./assets/specialDay/newYear/1.jpg",
-  "./assets/specialDay/newYear/2.jpg",
-  "./assets/specialDay/newYear/3.jpg",
-  "./assets/specialDay/newYear/4.jpg",
-  "./assets/specialDay/newYear/5.jpg",
+  "./assets/spring/1.webp",
+  "./assets/spring/2.webp",
+  "./assets/spring/3.webp",
+  "./assets/spring/4.webp",
+  "./assets/spring/5.webp",
+  "./assets/spring/6.webp",
+  "./assets/spring/7.webp",
+  "./assets/spring/8.webp",
+  "./assets/spring/9.webp",
+  "./assets/spring/10.webp",
+  "./assets/summer/1.webp",
+  "./assets/summer/2.webp",
+  "./assets/summer/3.webp",
+  "./assets/summer/4.webp",
+  "./assets/summer/5.webp",
+  "./assets/summer/6.webp",
+  "./assets/summer/7.webp",
+  "./assets/summer/8.webp",
+  "./assets/summer/9.webp",
+  "./assets/summer/10.webp",
+  "./assets/autumn/1.webp",
+  "./assets/autumn/2.webp",
+  "./assets/autumn/3.webp",
+  "./assets/autumn/4.webp",
+  "./assets/autumn/5.webp",
+  "./assets/autumn/6.webp",
+  "./assets/autumn/7.webp",
+  "./assets/autumn/8.webp",
+  "./assets/autumn/9.webp",
+  "./assets/autumn/10.webp",
+  "./assets/winter/1.webp",
+  "./assets/winter/2.webp",
+  "./assets/winter/3.webp",
+  "./assets/winter/4.webp",
+  "./assets/winter/5.webp",
+  "./assets/winter/6.webp",
+  "./assets/winter/7.webp",
+  "./assets/winter/8.webp",
+  "./assets/winter/9.webp",
+  "./assets/winter/10.webp",
+  "./assets/specialDay/christmas/1.webp",
+  "./assets/specialDay/christmas/2.webp",
+  "./assets/specialDay/christmas/3.webp",
+  "./assets/specialDay/christmas/4.webp",
+  "./assets/specialDay/christmas/5.webp",
+  "./assets/specialDay/halloween/1.webp",
+  "./assets/specialDay/halloween/2.webp",
+  "./assets/specialDay/halloween/3.webp",
+  "./assets/specialDay/halloween/4.webp",
+  "./assets/specialDay/halloween/5.webp",
+  "./assets/specialDay/newYear/1.webp",
+  "./assets/specialDay/newYear/2.webp",
+  "./assets/specialDay/newYear/3.webp",
+  "./assets/specialDay/newYear/4.webp",
+  "./assets/specialDay/newYear/5.webp",
 ];
 
 preloadImages(linkImg);
